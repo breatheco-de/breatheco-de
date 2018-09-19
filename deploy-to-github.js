16b915fde529781da1e7daa69f5832dbfc1b2d0d
Alejandro Sanchez <aalejo@gmail.com>
2018-09-19 03:31:31 +0000

    deploy to gihub updated

--- deploy-to-github.js
+++ deploy-to-github.js
@@ -3,12 +3,14 @@ var Console = require('bc-console');
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
+
 var remoteOriginUrl = require('remote-origin-url');
+var gh = require('parse-github-url');
 
 if (!fs.existsSync(path.resolve(__dirname,'.git'))){
     Console.error("No repository found on this project");
     Console.help("Follow this steps to create a new repository for your project: http://kbroman.org/github_tutorial/pages/init.html");
-        return;
+    return;
 }
 
 const origin = remoteOriginUrl.sync();
@@ -20,8 +22,11 @@ $ git remote get-url origin
 Add your remote by doing:
 $ git remote add origin <github_repository_url>
 `);
-    return;
-} 
+return;
+}
+Console.info("The remote was found successfully, starting the deploy from here: "+origin);
+
+const repository = gh(origin);
 const compiler = webpack(require(path.resolve(__dirname, 'webpack.config.js')));
 compiler.run((err, stats) => {
     if (err || stats.hasErrors()) {
@@ -38,7 +43,7 @@ compiler.run((err, stats) => {
             Console.error("There was an error publishing your website");
             return;
         } 
-        
-        Console.success("Your website has been deployed successfully");
+        //https://<github_user>.github.io/<repository-name>
+        Console.success(`Your website has been deployed successfully here: https://${repository["owner"]}.github.io/${repository["name"]}`);
     });
 });
