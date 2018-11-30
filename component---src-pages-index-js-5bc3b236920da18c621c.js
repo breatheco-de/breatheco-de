84c02253b00f2ef3978ac1727b97bd53e31ae937
Alejandro Sanchez <aalejo@gmail.com>
2018-11-30 18:26:35 +0000

    Updates

---
+++ component---src-pages-index-js-5bc3b236920da18c621c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{126:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-5bc3b236920da18c621c.js.map
