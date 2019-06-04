6796b04e886536de942b0d5ab7af069663714408
Alejandro Sanchez <aalejo@gmail.com>
2019-06-04 15:22:48 +0000

    updated home and gitpod integration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,33 @@
-import React from "react"
+import React from "react";
+import Link from 'gatsby-link';
+import { graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => {
+  const students = data.allStudentsYaml.edges;
+  console.log(students);
+  return (
+    <div>
+        <ul>
+            {
+                students.map(({ node }, i) => (<li key={i}><Link to={node.basic_info.github}>{node.basic_info.first_name + ' ' + node.basic_info.last_name}</Link></li>))
+            }
+        </ul>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query Index{
+    allStudentsYaml{
+        edges{
+            node{
+                basic_info{
+                    github
+                    first_name
+                    last_name
+                }
+            }
+        }
+    }
+  }
+`;
