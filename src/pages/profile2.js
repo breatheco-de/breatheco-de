ba98f3a1840bad3f8b849a044f762d56c0b3a397
yusniel Espino <espvtes@gmail.com>
2019-06-19 19:16:26 -0400

    other page

---
+++ src/pages/profile2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../templates/online-cv/styles/profile.css"
+import "bootstrap/dist/css/bootstrap.css";
+
+export default class Profile2 extends React.Component{
+    render(){
+        return(
+            <div className="container-fluid">
+                hello profile
+            </div>
+        );
+    }
+}
