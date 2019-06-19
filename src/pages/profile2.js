93e06c80c1c67f57546d403c495aea411714201a
yusniel Espino <espvtes@gmail.com>
2019-06-19 19:40:11 -0400

    sheet styles

--- src/pages/profile2.js
+++ src/pages/profile2.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import "../templates/online-cv/styles/profile.css"
+import "../templates/online-cv/styles/profile2.css"
 import "bootstrap/dist/css/bootstrap.css";
 
 export default class Profile2 extends React.Component{
     render(){
         return(
             <div className="container-fluid">
-                hello profile
+                <div className="row">
+                    <div className="col-12">
+                        <div className="parallax"></div>
+                    </div>
+                </div>
             </div>
         );
     }
