41e2e86b817522a916d7ca415fdaa00608627208
yusniel Espino <espvtes@gmail.com>
2019-06-20 23:48:37 -0400

    bubble

--- src/pages/profile2.js
+++ src/pages/profile2.js
@@ -25,49 +25,49 @@ export default class Profile2 extends React.Component{
                                         <a href="#section-1">
                                             <i className="fas fa-file-invoice fa-3x" />
                                         </a>
-                                        <span className="overlay">Summary</span>
+                                        <span className="overlay border"><b>SUMMARY</b></span>
                                     </li>
                                     <li className="border-bottom language">
                                         <a href="#section-2">
                                             <i className="fas fa-language fa-3x" />
                                         </a>
-                                        <span className="overlay">Language</span>
+                                        <span className="overlay border">LANGUAGE</span>
                                     </li>
                                     <li className="border-bottom interests">
                                         <a href="#section-3">
                                             <i className="fas fa-bicycle fa-3x" />
                                         </a>
-                                        <span className="overlay">Interests</span>
+                                        <span className="overlay border">INTERESTS</span>
                                     </li>
                                     <li className="border-bottom experiences">
                                         <a href="#section-4">
                                             <i className="fas fa-chart-line fa-3x" />
                                         </a>
-                                        <span className="overlay">Experiences</span>
+                                        <span className="overlay border">EXPERIENCES</span>
                                     </li>
                                     <li className="border-bottom project">
                                         <a href="#section-5">
                                             <i className="far fa-file-code fa-3x" />
                                         </a>
-                                        <span className="overlay">Project</span>
+                                        <span className="overlay border">PROJECT</span>
                                     </li>
                                     <li className="border-bottom skills">
                                         <a href="#section-6">
                                             <i className="far fa-star fa-3x" />
                                         </a>
-                                        <span className="overlay">Skills</span>
+                                        <span className="overlay border">SKILLS</span>
                                     </li>
                                     <li className="border-bottom education">
                                         <a href="#section-7">
                                             <i className="fas fa-graduation-cap fa-3x" />
                                         </a>
-                                        <span className="overlay">Education</span>
+                                        <span className="overlay border">EDUCATION</span>
                                     </li>
                                     <li className="contact">
                                         <a href="#section-8">
                                             <i className="fas fa-user-alt fa-3x" />
                                         </a>
-                                        <span className="overlay">Contact</span>
+                                        <span className="overlay border">CONTACT</span>
                                     </li>
                                 </ul>
                             </nav>
@@ -76,17 +76,19 @@ export default class Profile2 extends React.Component{
                             <div className="col-12 text-center">
                             <div className="container">
                                 <p className="font-weight-light" style={{fontSize:'105px', paddingTop:'34px'}}>JASON<strong className="font-weight-bold">&nbsp;STATHAM</strong></p>
-                                <p className="font-weight-light  border-bottom" style={{fontSize:'40px', marginTop:'-61px'}}>Full-Stack Developer</p>
+                                <p className="font-weight-light border-bottom" style={{fontSize:'40px', marginTop:'-61px'}}>Full-Stack Developer</p>
                             </div>
                             </div>
                         </div>
                         <div className="container">
                             <div className="row text-center">
                                 <div className="col-12 col-md-6">
-                                    <p className="font-italic">I like saving the world</p>
+                                    <p className="font-italic bubble">I like saving the world</p>
+                                    <p className="font-weight-light font-italic" style={{fontSize:'33px'}}>Motto</p>
                                 </div>
                                 <div className="col-12 col-md-6">
-                                    <p className="font-italic">Killing bad boys</p>
+                                    <p className="font-italic bubble">Killing bad boys</p>
+                                    <p className="font-weight-light font-italic" style={{fontSize:'33px'}}>Focus</p>
                                 </div>
                             </div>
 
