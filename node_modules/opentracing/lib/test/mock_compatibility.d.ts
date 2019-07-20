b7eeca8aa9554393ede362896caffac27dba9291
dimm8187@gmail.com <dimm8187@gmail.com>
2019-07-20 13:53:11 -0400

    removed node modules

--- node_modules/opentracing/lib/test/mock_compatibility.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function mockCompatibilityTests(): void;
-export default noopImplementationTests;
--- node_modules/opentracing/scripts/docker_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -t --rm --name test-runner-opentracing-javascript \
-    -v ${PWD}:/usr/src/opentracing -w /usr/src/opentracing \
-    node:$1 npm test
