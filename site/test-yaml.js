9224743b68c44925bd983f216fc7cdf248d7c3c1
Orlando <monroyorlando.36@gmail.com>
2020-04-30 18:06:07 -0400

    Fixed conflicts

--- site/test-yaml.js
+++ site/test-yaml.js
@@ -76,6 +76,21 @@ const validateProfiles = (profiles) => profiles.map(l => {
 
     if(githubs.includes(yaml.basic_info.github)) throw new Error(`Duplicated github username: ${yaml.basic_info.github.red} in two or more files`);
     githubs.push(yaml.basic_info.github);
+     //Only Advance, Intermediate and Basic measurements allowed 
+     for(let i = 0; i < yaml.skill.toolset.length; i++){
+        if(!/[a-zA-Z]/.test(yaml.skill.toolset[i]["level"])) throw new Error(`Invalid measurement unit in toolsets, Advanced, Intermediate and Basic are recommended`.red)
+      }
+      //Valid links in projects assigments
+      for(let i = 0; i < yaml.projects.assignments.length; i++){
+        if(!/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.test(yaml.projects.assignments[i]["link"])) throw new Error(`Invalid project link format`.red)
+      }
+      // Char limit and no "Bullet Point" allowed in the details paragraphs 
+      for(let i = 0; i < yaml.education.length; i ++ ){
+          if(yaml.education[i]["details"].length > 400 || !/\b(Bullet point)\b/.test(yaml.education[i]["details"])) throw new Error(`Your education detail is too long or "Bullet Point" is still in the paragraph`.red);
+      }
+      for(let i = 0; i < yaml.experiences.length; i ++ ){
+          if(yaml.experiences[i]["details"].length > 400 || !/\b(Bullet point)\b/.test(yaml.experiences[i]["details"])) throw new Error(`Your education detail is too long or "Bullet Point" is still in the paragraph`.red);
+      }
 
     return yaml;
 });
