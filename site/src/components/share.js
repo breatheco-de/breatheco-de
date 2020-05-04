78c8e17ec792c92a02676fa1e887e1de47515154
Alejandro Sanchez <aalejo@gmail.com>
2020-05-04 16:08:54 +0000

    backgroup for modal

--- site/src/components/share.js
+++ site/src/components/share.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Twitter, Facebook, Linkedin, Reddit } from 'react-social-sharing'
 
 const Share = ({ hide, message, url, onClose }) => {
-    return <div className={`modal ${hide ? "":"d-block"}`} tabindex="-1" role="dialog">
+    return <>
+        <div className={`modal ${hide ? "":"d-block"}`} tabindex="-1" role="dialog">
         <div className="modal-dialog" role="document">
             <div className="modal-content">
                 <div className="modal-header border-0 pb-0">
                     <h3 className="text-center mt-4">Share your commitment to code every day for 100 days:</h3>
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close"
                         onClick={() => onClose()}
-                    >
+                        >
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
@@ -25,6 +26,8 @@ const Share = ({ hide, message, url, onClose }) => {
             </div>
         </div>
     </div>
+    { hide === false && <div className="modal-backdrop" onClick={() => onClose()}></div> }
+    </>
 }
 
 export default Share;
