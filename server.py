be0e08b48775ea3edff989ba79247df096d0a55a
Alejandro Sanchez <aalejo@gmail.com>
2020-05-16 12:45:09 -0400

    Update server.py

--- server.py
+++ server.py
@@ -24,8 +24,8 @@ def serve_dir_directory_index():
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = os.path.join(path, 'index.html')
-        response = send_from_directory(static_file_dir, path)
-        response.cache_control.max_age = 0 # avoid cache memory
+    response = send_from_directory(static_file_dir, path)
+    response.cache_control.max_age = 0 # avoid cache memory
     return response
 
 app.run(host='0.0.0.0',port=3000, debug=True, extra_files=['./',])
